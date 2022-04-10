565fcad3e7b3026ea86061f8f790d2ee0825d436
Alexander Rothman <gnomesort@megate.ch>
2022-04-10 10:57:09 -0700

    Fix bugs with assertions.
    Fixes a variety of issues relateded to OBERON_ASSERT_MSG. This includes
    simple errors in the macro definition as well as improper use of the
    va_list in oberon::assert().

--- include/oberon/debug.hpp
+++ include/oberon/debug.hpp
@@ -24,9 +24,9 @@
   #define OBERON_ASSERT_MSG(x, msg, ...) \
     oberon::assert(std::source_location::current(), (x), (msg) __VA_OPT__(,) __VA_ARGS__)
 #else
-  #define OBERON_ASSERT(x) (void) ((x))
+  #define OBERON_ASSERT(x) ((void) (x))
 
-  #define OBERON_ASSERT_MSG(x, msg, ...) (void) ((x))
+  #define OBERON_ASSERT_MSG(x, msg, ...) ((void) (x))
 #endif
 
 #define OBERON_PRECONDITION(x) \
@@ -36,14 +36,14 @@
   OBERON_ASSERT(x)
 
 #define OBERON_PRECONDITION_MSG(x, msg, ...) \
-  OBERON_ASSERT(x, msg __VA_OPT__(,) __VA_ARGS__)
+  OBERON_ASSERT_MSG(x, msg __VA_OPT__(,) __VA_ARGS__)
 
 #define OBERON_POSTCONDITION_MSG(x, msg, ...) \
-  OBERON_ASSERT(x, msg, __VA_OPT__(,), __VA_ARGS__)
+  OBERON_ASSERT_MSG(x, msg, __VA_OPT__(,) __VA_ARGS__)
 
 namespace oberon {
 
-  void assert(const std::source_location& location, const bool condition, const std::string_view& message_format, ...);
+  void assert(const std::source_location& location, const bool condition, const std::string_view message_format, ...);
 
 }
 
--- src/oberon/debug.cpp
+++ src/oberon/debug.cpp
@@ -8,19 +8,22 @@
 #include <string>
 
 namespace oberon {
-  void assert(const std::source_location& location, const bool condition, const std::string_view& message_format, ...) {
+  void assert(const std::source_location& location, const bool condition, const std::string_view message_format, ...) {
     if (!condition)
     {
-      std::va_list args; // This is a special weirdo declaration.
+      // va_lists require this declaration style specifically
+      std::va_list args;
       // Calculate and allocate space for message.
-      auto sz = std::vsnprintf(nullptr, 0, std::data(message_format), args);
+      va_start(args, message_format);
+      std::va_list args_cp;
+      va_copy(args_cp, args); // The next call will destroy args so we need another copy.
+      auto sz = std::vsnprintf(nullptr, 0, std::data(message_format), args) + 1; // length + \0
+      va_end(args);
       auto message = std::string(sz, '\0');
-      std::vsnprintf(std::data(message), std::size(message), std::data(message_format), args);
-      std::fprintf(
-        stderr,
-        "%s:%" PRIu32 ": Assertion failed \"%s\"\n",
-        location.file_name(), location.line(), std::data(message)
-      );
+      std::vsnprintf(std::data(message), std::size(message), std::data(message_format), args_cp);
+      va_end(args_cp);
+      std::fprintf(stderr, "%s:%" PRIu32 ": Assertion failed \"%s\"\n", location.file_name(), location.line(),
+                   std::data(message));
       std::abort();
     }
   }
