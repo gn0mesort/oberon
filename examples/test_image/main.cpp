6c05adff485492188f61a4da238b218a21b5c1ba
Alexander Rothman <gnomesort@megate.ch>
2023-03-13 09:49:58 -0700

    Convert oberon into a library (liboberon).
    Converts the main project into a dynamic library and creates an
    executable example demonstrating the current output.
    
    This also defines the MESON_BUILD_DIRECTORY macro which can be used to
    locate the root build directory for file searchs.

--- examples/test_image/main.cpp
+++ examples/test_image/main.cpp
@@ -1,15 +1,10 @@
 /**
  * @file main.cpp
- * @brief Main application entry point.
+ * @brief "Test Image" example program.
  * @author Alexander Rothman <gnomesort@megate.ch>
  * @date 2023
  * @copyright AGPL-3.0+
  */
-#include <iostream>
-#include <iomanip>
-#include <fstream>
-#include <filesystem>
-
 #include "oberon/oberon.hpp"
 
 void toggle_fullscreen(oberon::window& win) {
@@ -34,41 +29,27 @@ void on_key_press(oberon::platform& plt, const oberon::u32, const oberon::key, c
   {
     toggle_fullscreen(win);
   }
-  if (inpt.key_is_just_pressed(oberon::key::character_d))
-  {
-    plt.graphics().dirty_renderer();
-  }
-}
-
-void on_window_resize(oberon::platform& plt, const oberon::window_extent& extent) {
-  std::cout << "Rebuilding Vulkan Swapchain with extent: { " << extent.width << ", " << extent.height
-            << " }" << std::endl;
-  plt.graphics().dirty_renderer();
-}
-
-void print_rect(const oberon::window_rect& rect) {
-  std::cout << "Current Rect: { {" << rect.offset.x << ", " << rect.offset.y << " }, { " << rect.extent.width << ", "
-            << rect.extent.height << " } }" << std::endl;
 }
 
 int app_run(const int, const oberon::ptr<oberon::csequence>, oberon::platform& plt) {
   auto& win = plt.window();
+  auto& gfx = plt.graphics();
   plt.attach_key_press_event_callback(on_key_press);
-  //plt.attach_window_resize_event_callback(on_window_resize);
   win.resize({ 1280, 720 });
   win.show();
-  plt.graphics().open_device(plt.graphics().preferred_device());
+  gfx.open_device(gfx.preferred_device());
   while (!win.quit_requested())
   {
     plt.drain_event_queue();
-    plt.graphics().begin_frame();
-    plt.graphics().draw_test_image();
-    plt.graphics().end_frame();
+    gfx.begin_frame();
+    gfx.draw_test_image();
+    gfx.end_frame();
   }
   return 0;
 }
 
 int main(int argc, char** argv) {
   auto app = oberon::application{ };
+  app.add_search_path(MESON_BUILD_DIRECTORY);
   return app.run(app_run, argc, argv);
 }
