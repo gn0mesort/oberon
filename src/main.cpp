25c85794a1e4e00239787fa2d118bd67dff56301
Alexander Rothman <gnomesort@megate.ch>
2023-03-13 08:00:26 -0700

    Update main function for new API changes.

--- src/main.cpp
+++ src/main.cpp
@@ -34,45 +34,35 @@ void on_key_press(oberon::platform& plt, const oberon::u32, const oberon::key, c
   {
     toggle_fullscreen(win);
   }
+  if (inpt.key_is_just_pressed(oberon::key::character_d))
+  {
+    plt.graphics().dirty_renderer();
+  }
 }
 
 void on_window_resize(oberon::platform& plt, const oberon::window_extent& extent) {
   std::cout << "Rebuilding Vulkan Swapchain with extent: { " << extent.width << ", " << extent.height
             << " }" << std::endl;
-  plt.graphics().reinitialize_renderer();
+  plt.graphics().dirty_renderer();
 }
 
-std::vector<char> read_shader_binary(const std::filesystem::path& file) {
-  auto f_in = std::ifstream{ file, std::ios::binary | std::ios::ate };
-  auto sz = f_in.tellg();
-  auto buffer = std::vector<char>(sz);
-  f_in.seekg(std::ios::beg);
-  f_in.read(buffer.data(), buffer.size());
-  return buffer;
+void print_rect(const oberon::window_rect& rect) {
+  std::cout << "Current Rect: { {" << rect.offset.x << ", " << rect.offset.y << " }, { " << rect.extent.width << ", "
+            << rect.extent.height << " } }" << std::endl;
 }
 
 int app_run(const int, const oberon::ptr<oberon::csequence>, oberon::platform& plt) {
   auto& win = plt.window();
   plt.attach_key_press_event_callback(on_key_press);
-  plt.attach_window_resize_event_callback(on_window_resize);
+  //plt.attach_window_resize_event_callback(on_window_resize);
   win.resize({ 1280, 720 });
   win.show();
-  auto test_frame = oberon::usize{ 0 };
-  {
-    auto vert_binary = read_shader_binary(plt.system().executable_path() / "data/oberon/shaders/test_frame.vert.spv");
-    auto vert_stage = plt.graphics().intern_pipeline_stage_binary(oberon::pipeline_stage::vertex, vert_binary);
-    auto frag_binary = read_shader_binary(plt.system().executable_path() / "data/oberon/shaders/test_frame.frag.spv");
-    auto frag_stage = plt.graphics().intern_pipeline_stage_binary(oberon::pipeline_stage::fragment, frag_binary);
-    auto program = oberon::render_program{ };
-    program.vertex_stage_id = vert_stage.id;
-    program.fragment_stage_id = frag_stage.id;
-    test_frame = plt.graphics().intern_render_program(program);
-  }
+  plt.graphics().open_device(plt.graphics().preferred_device());
   while (!win.quit_requested())
   {
     plt.drain_event_queue();
     plt.graphics().begin_frame();
-    plt.graphics().draw(3, test_frame);
+    plt.graphics().draw_test_image();
     plt.graphics().end_frame();
   }
   return 0;
