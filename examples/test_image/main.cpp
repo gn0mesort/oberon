f2fd9c8913c44e75725cd3fed7d492b0cc526de7
Alexander Rothman <gnomesort@megate.ch>
2023-03-20 17:48:59 -0700

    Update test_image example for new interfaces.

--- examples/test_image/main.cpp
+++ examples/test_image/main.cpp
@@ -48,26 +48,22 @@ void on_key_press(oberon::platform& plt, const oberon::u32, const oberon::key, c
 }
 
 int app_run(const int, const oberon::ptr<oberon::csequence>, oberon::platform& plt) {
-  auto& sys = plt.system();
   auto& win = plt.window();
   auto& gfx = plt.graphics();
-  sys.add_additional_search_path(MESON_BUILD_DIRECTORY);
   plt.attach_key_press_event_callback(on_key_press);
   win.resize({ 1280, 720 });
   win.show();
-  gfx.open_device(gfx.preferred_device());
-  gfx.request_presentation_mode(oberon::presentation_mode::fifo);
   while (!win.quit_requested())
   {
     plt.drain_event_queue();
-    gfx.begin_frame();
     gfx.draw_test_image();
-    gfx.end_frame();
+    gfx.submit_and_present_frame();
   }
   return 0;
 }
 
 int main(int argc, char** argv) {
   auto app = oberon::application{ };
+  app.add_initial_search_path(MESON_BUILD_DIRECTORY);
   return app.run(app_run, argc, argv);
 }
